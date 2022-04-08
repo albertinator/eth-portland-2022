22f51a8872d27bfb430eb6dfd4e223118ada2b71
Albert Ho <ah323@cornell.edu>
2022-04-08 16:22:40 -0700

    Update React app with connection to ETH address

--- trust_frontend/src/App.js
+++ trust_frontend/src/App.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from 'react';
 import Web3 from 'web3';
+import trustBuild from './Trust';
+const TRUST_ADDRESS = '0x860A7555653810d0F86b942563cFC4374e4E7a77';
+const TRUST_ABI = trustBuild.abi;
 
 function App() {
   const [account, setAccount] = useState();
+  const [trustContract, setTrustContract] = useState();
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
     async function load() {
@@ -10,6 +15,15 @@ function App() {
       const accounts = await web3.eth.requestAccounts();
 
       setAccount(accounts[0]);
+
+      // Instantiate smart contract using ABI and address.
+      const c = new web3.eth.Contract(TRUST_ABI, TRUST_ADDRESS);
+      setTrustContract(c);
+
+      // Instantiate balance of the trust
+      web3.eth.getBalance(TRUST_ADDRESS, (err, wei) => {
+        setBalance = web3.utils.fromWei(wei, 'ether')
+      })
     }
 
     load();
@@ -18,6 +32,8 @@ function App() {
   return (
     <div>
       This is the account: {account}
+      <br />
+      This is the balance: {balance}
     </div>
   );
 }
