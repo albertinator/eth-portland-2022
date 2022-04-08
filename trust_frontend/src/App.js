7385579929c891bc936367b5c7aa1ee1ecfcd6d9
Albert Ho <ah323@cornell.edu>
2022-04-08 14:19:25 -0700

    Add React app frontend hooked up to web3 127.0.0.1:7545

---
+++ trust_frontend/src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+
+function App() {
+  const [account, setAccount] = useState();
+
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.requestAccounts();
+
+      setAccount(accounts[0]);
+    }
+
+    load();
+  }, []);
+
+  return (
+    <div>
+      This is the account: {account}
+    </div>
+  );
+}
+
+export default App;
