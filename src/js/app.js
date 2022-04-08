30e466e9c587ad9dfd19da6e8a9128a737b874f5
Albert Ho <ah323@cornell.edu>
2022-04-08 16:24:20 -0700

    Add successful pull of trust data from web3

--- src/js/app.js
+++ src/js/app.js
@@ -4,21 +4,21 @@ App = {
 
   init: async function() {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    // $.getJSON('../pets.json', function(data) {
+    //   var petsRow = $('#petsRow');
+    //   var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+    //   for (i = 0; i < data.length; i ++) {
+    //     petTemplate.find('.panel-title').text(data[i].name);
+    //     petTemplate.find('img').attr('src', data[i].picture);
+    //     petTemplate.find('.pet-breed').text(data[i].breed);
+    //     petTemplate.find('.pet-age').text(data[i].age);
+    //     petTemplate.find('.pet-location').text(data[i].location);
+    //     petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+    //     petsRow.append(petTemplate.html());
+    //   }
+    // });
 
     return await App.initWeb3();
   },
@@ -52,23 +52,56 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('Adoption.json', function(data) {
+    $.getJSON('Trust.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with @truffle/contract
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      var TrustArtifact = data;
+      App.contracts.Trust = TruffleContract(TrustArtifact);
 
       // Set the provider for our contract
-      App.contracts.Adoption.setProvider(App.web3Provider);
+      App.contracts.Trust.setProvider(App.web3Provider);
 
       // Use our contract to retrieve and mark the adopted pets
-      return App.markAdopted();
+      return App.getTrustInfo();
     });
 
     return App.bindEvents();
   },
 
+  getTrustInfo: function() {
+    var trustInstance;
+
+    var creator;
+    var heir;
+    var usState;
+    var transferDate;
+
+    App.contracts.Trust.deployed().then(function(instance) {
+      trustInstance = instance;
+
+      // return {
+      //   creator: trustInstance.creator.call(),
+      //   heir: trustInstance.heir.call(),
+      //   usState: trustInstance.usState.call(),
+      //   transferDate: trustInstance.transferDate.call(),
+      // };
+      return trustInstance.creator.call();
+    }).then(function(creator) {
+      $('.creator').text("Creator: " + creator);
+      return trustInstance.heir.call();
+    }).then(function(heir) {
+      $('.heir').text("Heir: " + heir);
+      return trustInstance.usState.call();
+    }).then(function(usState) {
+      $('.usState').text("US State: " + usState);
+      return trustInstance.transferDate.call();
+    }).then(function(transferDate) {
+      $('.transferDate').text("Transfer Date: " + transferDate);
+    })
+  },
+
   bindEvents: function() {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '.btn-create-trust', App.handleCreateTrust);
   },
 
   markAdopted: function() {
@@ -110,6 +143,32 @@ App = {
         return adoptionInstance.adopt(petId, {from: account});
       }).then(function(result) {
         return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  handleCreateTrust: function(event) {
+    event.preventDefault();
+
+    var trustInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Trust.deployed().then(function(instance) {
+        trustInstance = instance;
+
+        // Execute init as a transaction by sending account
+        // return trustInstance.adopt(petId, {from: account});
+        $('.creator').text('CREATED!')
+      }).then(function(result) {
+        // return App.markAdopted();
       }).catch(function(err) {
         console.log(err.message);
       });
